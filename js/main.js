6c69f423b8db21d2dc4a5246c4bab3b28718bb77
rahadian <anrahadi02@gmail.com>
2026-08-03 11:47:01 +0800

    adding analyzing features fixing ui and adding animations

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-
 const imageInput = document.getElementById("imageInput");
 const preview = document.getElementById("preview");
 const uploadIcon = document.querySelector(".upload-icon");
 const uploadTitle = document.querySelector(".upload-title");
 const uploadText = document.querySelector(".upload-text");
+const analyzeBtn = document.getElementById("analyze-btn")
+const loading = document.getElementById("loading");
+const progress = document.querySelector(".progress");
+const loadingText = document.getElementById("loading-text");
+
 
 imageInput.addEventListener("change", function(){
 
@@ -14,11 +18,81 @@ imageInput.addEventListener("change", function(){
     preview.src = URL.createObjectURL(file);
 
     preview.style.display = "block";
+    analyzeBtn.style.display = "block";
     uploadIcon.style.display = "none";
     uploadTitle.style.display = "none";
     uploadText.style.display = "none";
 
     }
+
+});
+
+analyzeBtn.addEventListener("click", function(){
+    const loading = document.getElementById("loading");
+
+    loading.style.display = "block";
+    
+    analyzeBtn.disabled = true;
+    analyzeBtn.textContent = "analyzing...";
+
+    let percent = 0;
+
+    const progress = document.querySelector(".progress")
+    const interval = setInterval(function(){
+
+        percent ++;
+
+        progress.style.width = percent + "%";
+        loadingText.textContent = percent + "%";
+
+        
+        if (percent >=100){
+
+            clearInterval(interval);
+
+            setTimeout(function(){
+
+                loading.style.display = "none";
+
+                progress.style.width = "0%";
+                loadingText.textContent = "0%";
+
+                preview.src = "";
+                preview.style.display = "none";
+
+                analyzeBtn.style.display = "none";
+                analyzeBtn.disabled = false;
+
+                uploadIcon.style.display = "block";
+                uploadTitle.style.display = "block";
+                uploadText.style.display = "block";
+
+                imageInput.value = "";
+
+                analyzeBtn.textContent = "Analyze My Face"
+
+            }, 1500);
+
+            loadingText.textContent = "✅ Analysis Complete!";
+
+            analyzeBtn.disabled = false;
+        }else if(percent <20){
+
+            loadingText.textContent = "📷 Reading image..." + percent + "%";
+        }else if(percent >25){
+
+            loadingText.textContent = "🔍 Detecting facial landmarks..." + percent + "%";
+        }else if(percent <45){
+
+            loadingText.textContent = "📐 Measuring facial symmetry..." + percent + "%";
+        }else if(percent >75){
+
+            loadingText.textContent = "🧠 Generating AI report..."; + percent + "%";
+        }
+
+    },40);
+
+ 
 
 });
 
